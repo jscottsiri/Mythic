f6e9699b2f90442a24935424254eb29366147bae
jscottsiri <jss82@njit.edu>
2018-10-09 16:25:26 -0400

    Node scanning incoming shortly

---
+++ Assets/Script/Nodemaker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class Nodemaker : MonoBehaviour {
+
+    //Objects Required
+    public Grid thegrid;
+    public Tilemap tmap;
+    public List<Tilemap> objectlayers;
+    public GameObject Nodefab;
+
+    //Map Bounds
+    public int scanStartX;
+    public int scanEndX;
+    public int scanStartY;
+    public int scanEndY;
+
+    //Nodes that Interact
+    public List<GameObject> allnodes;
+    public GameObject[,] nodegrid;
+	// Use this for initialization
+	void Awake () {
+        //initialize allnodes
+        allnodes = new List<GameObject>();
+	}
+	
+    public void generateNodes()
+    {
+        //what you call to actually make nodes
+        //createNodes();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void createNodes()
+    {
+
+    }
+}
