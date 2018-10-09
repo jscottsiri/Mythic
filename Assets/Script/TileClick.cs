01f26846a2eea382ed4aee80cfb7b587e09a9fe2
jscottsiri <jss82@njit.edu>
2018-10-09 03:14:42 -0400

    Player Pathfinding Testing Imminent

---
+++ Assets/Script/TileClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileClick : MonoBehaviour {
+    public GameObject Selector;
+
+    Vector3 posit;
+    void OnMouseDown()
+    {
+        //get mouse position
+        posit = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //convert to proper placement
+        posit.x = Mathf.Floor(posit.x) + 0.5f;
+        posit.y = Mathf.Floor(posit.y) + 0.5f;
+        posit.z = 0;
+
+        //move selector
+        Selector.transform.position = posit;
+        Debug.Log(posit.x + ", " + posit.y);
+    }
+}
---
+++ Assets/Script/TileStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileStats : MonoBehaviour {
+    //States
+    public bool current = false;
+    public bool target = false;
+    public bool selectable = false;
+
+    public int movementweight;
+
+    public GameObject Selector;
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target)
+        {
+        }
+        else if (target)
+        {
+
+        }
+        else if (selectable)
+        {
+
+        }
+        else
+        {
+
+        }
+	}
+}
